df052b964107c59c6c50886fc72ae4c21481ec2f
Kristofer Norman <kjn2418@email.vccs.edu>
2024-08-11 22:51:58 -0400

    Add project files.

---
+++ NormanKnows/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NormanKnows.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
